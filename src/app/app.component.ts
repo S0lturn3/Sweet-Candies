71d4d45e6f621b6cb12eb558d9eb130bc82903b4
ERICK  Carvalho Paulette de Oliveira <ecoliveira@sispro.com.br>
2021-11-25 14:21:04 -0300

    v1.0.3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
-import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject, OnInit, Renderer2 } from '@angular/core';
+import { inject } from '@angular/core/testing';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Sweet Candies | Bakery';
+  theme: Theme = 'dark-theme';
+
+  constructor(
+    @Inject(DOCUMENT) private document: Document,
+    private renderer: Renderer2
+  ) { }
+
+  ngOnInit() {
+    this.initializeTheme();
+  }
+
+  switchTheme(){
+    this.document.body.classList.replace(
+      this.theme,
+       this.theme === 'dark-theme'
+       ? (this.theme = 'light-theme')
+       : (this.theme = "dark-theme"))
+  }
+
+  initializeTheme = (): void =>
+    this.renderer.addClass(this.document.body, this.theme);
 }
+
+export type Theme = 'dark-theme' | 'light-theme';
--- src/app/theme-toggle/theme-toggle.component.ts
+++ src/app/theme-toggle/theme-toggle.component.ts
@@ -1,21 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'theme-toggle',
   templateUrl: './theme-toggle.component.html',
-  styleUrls: ['./theme-toggle.component.css']
+  styleUrls: ['./theme-toggle.component.scss']
 })
 export class ThemeToggleComponent implements OnInit {
 
-  // faCoffe = faCoffee;
+  @Output() changeTheme = new EventEmitter();
 
   moonICO = faMoon;
   sunICO = faSun;
 
-  constructor() {}
-
-  ngOnInit(): void {
+  switchTheme(){
+    this.changeTheme.emit()
   }
 
+  constructor() { }
+
+  ngOnInit(): void { }
+
 }
+// ----- Running fine
